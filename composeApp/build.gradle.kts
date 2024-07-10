67d6d8886b72c39961c6953dd6c21f7f584265b1
HARSH <theharsh1210@gmail.com>
2024-07-10 16:27:34 +0530

    change

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -61,22 +61,29 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+//    packaging {
+//        resources {
+//            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+//        }
+//    }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
     }
+//    compileOptions {
+//        sourceCompatibility = JavaVersion.VERSION_11
+//        targetCompatibility = JavaVersion.VERSION_11
+//    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     buildFeatures {
         compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
     dependencies {
         debugImplementation(compose.uiTooling)
