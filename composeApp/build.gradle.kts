b30dac82c65e4ddb76e2c66b2392dbd589b50d88
HARSH <theharsh1210@gmail.com>
2024-07-11 22:42:51 +0530

    jai

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,7 +43,6 @@ kotlin {
             implementation(libs.androidx.activity.compose)
         }
         commonMain.dependencies {
-            implementation(kotlin("stdlib"))
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
