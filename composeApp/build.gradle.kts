a058a9575146fb986bd567f34784a796f55e5a1d
HARSH <theharsh1210@gmail.com>
2024-07-11 14:45:08 +0530

    update

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -19,7 +19,7 @@ kotlin {
     androidTarget {
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
     
@@ -74,16 +74,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
     }
-
-//    dependencies {
-//        debugImplementation(compose.uiTooling)
-//    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    dependencies {
+        debugImplementation(compose.uiTooling)
+    }
 }
 publishing {
     publications{
